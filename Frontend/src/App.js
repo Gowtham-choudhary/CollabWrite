579390bb751f51f939bb7737d98a999becb25d30
Gowtham <163446882+Gowtham-choudhary@users.noreply.github.com>
2024-12-02 22:59:23 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" to={`/documents/${uuidV4()}`}></Route>
+        <Route path="/documents/:id" element={<TextEditor />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
